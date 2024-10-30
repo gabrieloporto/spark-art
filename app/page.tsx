e9b2802d704e734a0ebd1a9f7c79244c8914b921
Gabriel Oporto <gabyoporto@gmail.com>
2024-10-30 16:49:57 -0300

    fixing dimension issues at page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -29,11 +29,85 @@ export default function Home() {
 
   return (
     <>
-      <header className="flex justify-between items-center">
-        <Image alt="logo" src="/logo.webp" width="100" height="100" />
+      <header className="flex justify-between items-center mt-8">
+        <Image alt="logo" src="/logo.webp" width={80} height={80} />
         {imageUrl && !loading ? (
-          <a href={imageUrl} download="imagen-generada.jpg">
-            <button className="bg-[#577da0] p-4 hover:bg-[#446485] rounded-sm">
+          <a
+            href={imageUrl}
+            className="bg-[#577da0] p-4 hover:bg-[#446485] rounded-sm"
+            download="image-generate.jpg"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+              <path d="M7 11l5 5l5 -5" />
+              <path d="M12 4l0 12" />
+            </svg>
+          </a>
+        ) : null}
+      </header>
+
+      {loading ? (
+        <div className="flex items-center justify-center">
+          <div className="flex items-center flex-col justify-center w-full max-w-md h-80 bg-[#a7bdd2] animate-pulse rounded shadow-lg" />
+        </div>
+      ) : imageUrl ? (
+        <div className="flex items-center justify-center">
+          <div className="flex items-center flex-col justify-center w-full max-w-md h-80">
+            <img
+              src={imageUrl}
+              alt="Imagen Generada"
+              className="w-full h-full object-cover rounded shadow-lg"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center w-full h-80 bg-[#293747]">
+          <p className="text-center text-xl text-white p-4">
+            <strong className="text-2xl">¡Te doy la Bienvenida!</strong>
+            <br />
+            <br />
+            Describe tu idea,
+            <br />
+            <strong className="text-[#84F2EE]">Spark</strong> se encargara de
+            convertirla en una imagen.
+            <br />
+          </p>
+        </div>
+      )}
+
+      <form className="flex items-center justify-center rounded-full w-full mb-8">
+        <input
+          type="text"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder="Escribe tu idea"
+          className={`text-black pl-6 py-2 w-full rounded-l-full ${
+            loading && "opacity-50"
+          }`}
+          disabled={loading}
+        />
+        <button
+          onClick={handleGenerateImage}
+          className={`bg-[#577da0] px-4 py-2 rounded hover:bg-[#446485] rounded-r-full ${
+            loading && "opacity-50"
+          }`}
+          disabled={loading}
+        >
+          {loading ? (
+            "Generando..."
+          ) : (
+            <>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -41,76 +115,21 @@ export default function Home() {
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="icon icon-tabler icons-tabler-outline icon-tabler-download"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-                <path d="M7 11l5 5l5 -5" />
-                <path d="M12 4l0 12" />
+                <path d="M15 8h.01" />
+                <path d="M12 21h-6a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v6" />
+                <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l3.993 3.993" />
+                <path d="M14 14l1 -1c.47 -.452 .995 -.675 1.52 -.67" />
+                <path d="M19 22.5a4.75 4.75 0 0 1 3.5 -3.5a4.75 4.75 0 0 1 -3.5 -3.5a4.75 4.75 0 0 1 -3.5 3.5a4.75 4.75 0 0 1 3.5 3.5" />
               </svg>
-            </button>
-          </a>
-        ) : (
-          ""
-        )}
-      </header>
-
-      <div className="flex items-center flex-col justify-center">
-        {loading ? (
-          <div
-            className={`w-[412px] sm:w-[620px] md:w-[768px] lg:w-[1080px] h-[412px] sm:h-[620px] md:h-[768px] lg:h-[1080px] bg-[#a7bdd2] animate-pulse rounded shadow-lg mt-8`}
-          />
-        ) : imageUrl ? (
-          <div>
-            <img
-              id="img"
-              src={imageUrl}
-              alt="Imagen Generada"
-              className="w-[412px] sm:w-[620px] md:w-[768px] lg:w-[1080px] rounded shadow-lg"
-            />
-          </div>
-        ) : (
-          <div className="w-[412px] sm:w-[620px] md:w-[768px] lg:w-[1080px] flex items-center justify-center bg-[#293747] rounded m-8">
-            <p className="text-center p-8">
-              ¡Bienvenido a SparkArt!
-              <br />
-              Describe tu idea, y nuestro sistema la convertirá en una imagen
-              usando inteligencia artificial.
-            </p>
-          </div>
-        )}
-      </div>
-
-      <form className="flex items-center justify-center rounded-full mb-4 w-full">
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Escribe tu idea"
-          className={`text-black pl-[10%] sm:pl-[8%] md:pl-[6%] py-5 w-full rounded-l-full ${
-            loading && "opacity-50"
-          }`}
-          disabled={loading}
-        />
-        <button
-          onClick={handleGenerateImage}
-          className={`bg-[#577da0] px-4 py-2 rounded hover:bg-[#446485] rounded-r-full  ${
-            loading && "opacity-50"
-          }`}
-          disabled={loading}
-        >
-          {loading ? "Generando..." : "Crear Imagen"}
+            </>
+          )}
         </button>
       </form>
-
-      <footer className="flex items-center justify-center text-sm">
-        Este sitio hace uso de
-        <strong className="text-[#7899b8] pl-1">Inteligencia Artificial</strong>
-        .
-      </footer>
     </>
   );
 }
