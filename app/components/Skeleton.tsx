e1e4419378b90cc6473d38bd04c715a9c266374b
Gabriel Oporto <gabyoporto@gmail.com>
2024-10-31 12:01:30 -0300

    Create Skeleton component

---
+++ app/components/Skeleton.tsx
@@ -0,0 +1,7 @@
+export default function Skeleton() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="flex items-center flex-col justify-center w-full max-w-md h-80 bg-[#a7bdd2] animate-pulse rounded shadow-lg" />
+    </div>
+  );
+}
