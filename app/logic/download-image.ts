3fa65fbf32598a0fde086d7cb396622f01107d17
Gabriel Oporto <gabyoporto@gmail.com>
2024-10-31 12:03:38 -0300

    creating dowloadImage function

---
+++ app/logic/download-image.ts
@@ -0,0 +1,22 @@
+import { DownloadSparkImage } from "../lib/types";
+
+export default async function downloadImage({
+  imageUrl,
+  promptRef,
+}: DownloadSparkImage) {
+  try {
+    const response = await fetch(imageUrl);
+    const blob = await response.blob();
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `SparkArt_${promptRef.current}.jpg`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error("Error downloading image:", error);
+  }
+}
