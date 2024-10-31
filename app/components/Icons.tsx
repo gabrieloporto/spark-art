c97e8d4885095e524617d3eed0af960e296349f7
Gabriel Oporto <gabyoporto@gmail.com>
2024-10-31 16:33:45 -0300

    Add Error Icon component

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -79,3 +79,24 @@ export const DownloadIcon = () => {
     </svg>
   );
 };
+
+export const ErrorIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ef4444"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" />
+      <path d="M12 8v4" />
+      <path d="M12 16h.01" />
+    </svg>
+  );
+};
